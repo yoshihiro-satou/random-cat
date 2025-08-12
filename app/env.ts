609845098032f71d68e56110bfbc6b5d683b0902
yoshihiro satou <128808496+yoshihiro-satou@users.noreply.github.com>
2025-08-12 20:17:02 +0900

    Update CAT_API_KEY to use new env variable
    Replaces usage of CAT_API_KEY with NEXT_SERVER_ACTIONS_ENCRYPTION_KEY in environment variable checks and exports. Updates error message accordingly.

--- app/env.ts
+++ app/env.ts
@@ -1,6 +1,6 @@
 import { error } from "console";
 
-if(!process.env.CAT_API_KEY) {
+if(!process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY) {
   throw error("環境変数CAT_API_KEY が設定されていません");
 }
-export const CAT_API_KEY = process.env.CAT_API_KEY;
+export const CAT_API_KEY = process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY;
